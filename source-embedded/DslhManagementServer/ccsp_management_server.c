947b23e9e5a43c5c965467b81607cbf00a48f8e3
Ajay_Bhat01 <abhat.contractor@libertyglobal.com>
2020-07-28 20:09:28 +0200

    Wait for the IP to be assigned to the CWMP(erouter) interface before the ACS tries to contact the ACS
    FIX DESCRIPTION:
    1. Wait for the IP to be assigned to the CWMP(erouter) interface before
       the ACS tries to contact the ACS
    2. Firewall to restart after EnableCWMP toggle.
    
    TESTING PERFORMED:
    1. Setup the ACS connectivity.
    2. Verify that the device is coming online on the ACS.
    3. Verify the periodic inform is sent from the DUT only after CWMP
       interface has an IP.
    
    Signed-off-by: Ajay_Bhat01 <abhat.contractor@libertyglobal.com>

--- source-embedded/DslhManagementServer/ccsp_management_server.c
+++ source-embedded/DslhManagementServer/ccsp_management_server.c
@@ -2286,6 +2286,7 @@ int CcspManagementServer_ValidateParameterValues(
                 switch (parameterID)
                 {
                 case ManagementServerEnableCWMPID:
+                     system("firewall restart");
                 //case ManagementServerPeriodicInformEnableID:
                 case ManagementServerACSOverrideID:
                 case ManagementServerUpgradesManagedID:

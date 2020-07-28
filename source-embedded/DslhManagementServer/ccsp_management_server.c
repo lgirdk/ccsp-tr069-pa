f5891966c16511cc614cc535b968a7807d6c6a4b
Ajay_Bhat01 <abhat.contractor@libertyglobal.com>
2020-07-28 20:09:28 +0200

    Wait for the IP to be assigned to the CWMP(erouter) interface before the ACS tries to contact the ACS
    FIX DESCRIPTION:
    1. Wait for the IP to be assigned to the CWMP(erouter) interface before
       the ACS tries to contact the ACS
    2. Firewall to restart after EnableCWMP toggle.
    
    TESTING PERFORMED:
    1. Setup the ACS connectivity.
    2. Verify that the device is coming online on the ACS.
    3. Verify the periodic inform is sent from the DUT only after CWMP
       interface has an IP.
    
    Signed-off-by: Ajay_Bhat01 <abhat.contractor@libertyglobal.com>
    
    fix for issue when the firewall rules for tr69 is not updated when CWMP is Enabled or Disabled through dmcli ( OFW-20 )
    
    BUG NUMBER: OFW-20, RDKB-4649
    
    FIX DESCRIPTION:
    1. Fix for issue when the firewall rules for tr69 is not updated
       when CWMP is Enabled or Disabled through dmcli.
    2. The firewall was being restarted before the PSM was
       updated with the new value of the EnableCWMP Paramter.
    
    TESTING PERFORMED:
    1. Setup the ACS connectivity.
    2. Verify that the device is coming online on the ACS.
    3. Check for iptable rules being updated using
       "iptables -S | grep tr69".
    4. Ensure that the CWMP Port configured is open.
    5. Disable CWMP using dmcli.
    6. Repeat step 3 to ensure that the CWMP Port is
       no longer open.
    
    Signed-off-by: Ajay_Bhat01 <abhat.contractor@libertyglobal.com>

--- source-embedded/DslhManagementServer/ccsp_management_server.c
+++ source-embedded/DslhManagementServer/ccsp_management_server.c
@@ -2936,6 +2936,7 @@ int CcspManagementServer_CommitParameterValues(unsigned int writeID)
     size_t len1, len2, len3;
     int diagComplete = 0;
     errno_t rc = -1;
+    int frestart = 0;
 
     len1 = strlen(CcspManagementServer_ComponentName);
     slapVar.Syntax = SLAP_VAR_SYNTAX_string;
@@ -2967,6 +2968,10 @@ int CcspManagementServer_CommitParameterValues(unsigned int writeID)
         if ( objectID == ManagementServerID && parameterID == ManagementServerX_CISCO_COM_DiagCompleteID )
         {
             diagComplete = 1;
+        }
+        if ( objectID == ManagementServerID && ( parameterID == ManagementServerEnableCWMPID || parameterID == ManagementServerX_CISCO_COM_ConnectionRequestURLPortID))
+        {
+                frestart = 1;
         }
 #ifdef USE_NOTIFY_COMPONENT
 	if( objectID == NotifyID)
@@ -3075,6 +3080,11 @@ int CcspManagementServer_CommitParameterValues(unsigned int writeID)
             if(parameterID == LoggingEnableID) CcspManagementServer_SetLogging_EnableStr(NULL, objectInfo[objectID].parameters[parameterID].value);
             else if(parameterID == LoggingLogLevelID) CcspManagementServer_SetLogging_LogLevelStr(NULL, objectInfo[objectID].parameters[parameterID].value);
         }
+    }
+
+    if( frestart == 1 )
+    {
+        system("firewall restart");
     }
 
     /* Inform PA about the data change. */

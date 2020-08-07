fc7f19552bcd0c345f0d8e3e9dbedca5e302ef41
Gokulakrishnan Ramalingam (Infosys) <gramalingam.contractor@libertyglobal.com>
2020-08-07 12:32:07 +0200

    fixed a memory leak in CcspTr069PaSsp_LoadCfgFile() etc
    BUG NUMBER: RDKB-4578
    
    FIX DESCRIPTION:
    	1. Fixed a memory leak in CcspTr069PaSsp_LoadCfgFile() API
    	2. Device is not reporting ScheduleInform in the list of supported RPC's
               in response to the GetRPCMethods call. The root cause of the issue
               is that RDKB TR069 code has CCSP_CWMP_CFG_RPC_METHOD_NO_ScheduleInform
               included in CcspTr069PaSsp_CcspCwmpCfgNoRPCMethods. The fix is deleting
               CCSP_CWMP_CFG_RPC_METHOD_NO_ScheduleInform from the NoRPCMethod function.
    
    TESTING PERFORMED:
    	1. Setup the ACS connectivity.
    	2. Verify that the device is coming online on the ACS.
    	3. Try to perform a GetRPC from the ACS.
    	4. Schedule Inform should be part of the supported method.
    
    Signed-off-by: Gokulakrishnan Ramalingam <gramalingam.contractor@libertyglobal.com>

--- source-embedded/Ssp/ssp_ccsp_cwmp_cfg.c
+++ source-embedded/Ssp/ssp_ccsp_cwmp_cfg.c
@@ -349,8 +349,8 @@ CcspTr069PaSsp_LoadCfgFile
     /*
      *  Parse the XML content
      */
+    PANSC_XML_DOM_NODE_OBJECT       pRootNode          = (PANSC_XML_DOM_NODE_OBJECT)NULL;
     {
-        PANSC_XML_DOM_NODE_OBJECT       pRootNode          = (PANSC_XML_DOM_NODE_OBJECT)NULL;
         PANSC_XML_DOM_NODE_OBJECT       pChildNode         = (PANSC_XML_DOM_NODE_OBJECT)NULL;
         char*                           pXMLIterator       = pXMLContent;
         
@@ -402,6 +402,9 @@ CcspTr069PaSsp_LoadCfgFile
     }
     
 EXIT:
+    if (pRootNode) {
+        AnscXmlDomNodeRemove(pRootNode);
+    }
     AnscFreeMemory(pXMLContent);
     return  returnStatus;
 }
@@ -442,7 +445,6 @@ CcspTr069PaSsp_CcspCwmpCfgNoRPCMethods
         CCSP_CWMP_CFG_RPC_METHOD_NO_GetQueuedTransfers   |
         CCSP_CWMP_CFG_RPC_METHOD_NO_SetVouchers          |
         CCSP_CWMP_CFG_RPC_METHOD_NO_GetOptions           |
-        CCSP_CWMP_CFG_RPC_METHOD_NO_ScheduleInform       |
         CCSP_CWMP_CFG_RPC_METHOD_NO_Upload;
 
     return ulNoMethods;

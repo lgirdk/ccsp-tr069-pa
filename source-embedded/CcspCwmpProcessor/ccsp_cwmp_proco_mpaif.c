575622bc130150d2fbe9eb2cf5b7fb8fd4006d8f
Andre McCurdy <armccurdy@gmail.com>
2020-12-03 11:04:35 -0800

    fix error code for an RPC method for a non existing index ( OFW-313 )
    Follow up to the previous patch to fix additional conditions.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_mpaif.c
+++ source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_mpaif.c
@@ -2205,7 +2205,7 @@ CcspCwmppoMpaGetParameterValues
             if ( nResult != CCSP_SUCCESS )
             {
             	CcspTr069PaTraceDebug(("GPV failure on FC %s, error = %d\n", pFcNsList->FCName, nResult));
-                nCcspError = CCSP_SUCCESS;
+                nCcspError = nResult;
                 returnStatus = ANSC_STATUS_INTERNAL_ERROR;
                 break;
             }
@@ -2499,7 +2499,7 @@ EXIT2:
         if ( nCcspError != CCSP_SUCCESS )
         {
             CCSP_INT                nCwmpError;
-            
+
             nCwmpError = CcspTr069PA_MapCcspErrCode(pCcspCwmpCpeController->hTr069PaMapper, nCcspError);
 
             CCSP_CWMP_SET_SOAP_FAULT(pCwmpSoapFault, nCwmpError);
@@ -2650,7 +2650,7 @@ CcspCwmppoMpaGetParameterNames
     char*                           pRootObjName        = pCcspCwmpCpeController->GetRootObject((ANSC_HANDLE)pCcspCwmpCpeController);
     ULONG                           i                   = 0;
     QUEUE_HEADER                    FcGpnResultListQueue;
-    int                             nRet;
+    int                             nRet = CCSP_SUCCESS;
     char**                          ppFcNameArray        = NULL;
     char**                          ppDbusPathArray      = NULL;
     char**                          ppSubsysArray        = NULL;
@@ -2674,6 +2674,7 @@ CcspCwmppoMpaGetParameterNames
     PCCSP_TR069PA_STRING_SLIST_ENTRY pSListEntry;
     PSINGLE_LINK_ENTRY              pLink;
     CCSP_STRING                     InternalName;
+    CCSP_INT                        nCcspError           = CCSP_SUCCESS;
 
     *ppParamInfoArray = NULL;
     *pulArraySize     = 0;
@@ -2860,6 +2861,7 @@ CcspCwmppoMpaGetParameterNames
         if ( nRet != CCSP_SUCCESS )
         {
             CcspTr069PaTraceDebug(("GPN - FC <%s> returned error %d.\n", ppFcNameArray[i], nRet));
+            nCcspError = nRet;
             continue;
         }
         else
@@ -3037,7 +3039,14 @@ EXIT2:
 
     if ( pCwmpSoapFault )
     {
-        if ( returnStatus == ANSC_STATUS_RESOURCES )
+        if ( nCcspError != CCSP_SUCCESS )
+        {
+          CCSP_INT nCwmpError;
+
+          nCwmpError = CcspTr069PA_MapCcspErrCode(pCcspCwmpCpeController->hTr069PaMapper, nCcspError);
+          CCSP_CWMP_SET_SOAP_FAULT(pCwmpSoapFault, nCwmpError);
+        }
+        else if ( returnStatus == ANSC_STATUS_RESOURCES )
         {
             CCSP_CWMP_SET_SOAP_FAULT(pCwmpSoapFault, CCSP_CWMP_CPE_CWMP_FaultCode_resources);
         }
@@ -3164,6 +3173,7 @@ CcspCwmppoMpaSetParameterAttributes
     PCCSP_TR069PA_FC_NSLIST         pFcNsList            = NULL;
     char*                           pParamName           = NULL;
     PCCSP_TR069PA_NSLIST            pNsList              = NULL;
+    CCSP_INT                        nCcspError           = CCSP_SUCCESS;
 
     BOOL                            bIncludeInvQuery     = !bExcludeInvNs;
     QUEUE_HEADER                    ParamList={{0},0,{0}};
@@ -3422,6 +3432,7 @@ CcspCwmppoMpaSetParameterAttributes
 
             if ( nResult != CCSP_SUCCESS )
             {
+                nCcspError = nResult;
             	CcspTr069PaTraceDebug(("SPA failure on FC %s, error = %d, ignored.\n", pFcNsList->FCName, nResult));
                 if ( nResult >= CCSP_ERR_NOT_CONNECT && nResult <= CCSP_ERR_NOT_SUPPORT )
                 {
@@ -3458,7 +3469,14 @@ EXIT2:
 
     if ( pCwmpSoapFault )
     {
-        if ( returnStatus == ANSC_STATUS_RESOURCES )
+        if ( nCcspError != CCSP_SUCCESS )
+        {
+          CCSP_INT nCwmpError;
+
+          nCwmpError = CcspTr069PA_MapCcspErrCode(pCcspCwmpCpeController->hTr069PaMapper, nCcspError);
+          CCSP_CWMP_SET_SOAP_FAULT(pCwmpSoapFault, nCwmpError);
+        }
+        else if ( returnStatus == ANSC_STATUS_RESOURCES )
         {
             CCSP_CWMP_SET_SOAP_FAULT(pCwmpSoapFault, CCSP_CWMP_CPE_CWMP_FaultCode_resources);
         }

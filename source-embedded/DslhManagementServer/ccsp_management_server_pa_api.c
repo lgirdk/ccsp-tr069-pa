48f4270d9b23c1ef8c906223efb7dbb99385db22
subhradeep <sunath.contractor@libertyglobal.com>
2020-12-16 08:17:32 +0100

    OFW-21 : Device.ManagementServer.URL should have value of configured URL if ACSOverride=0 in boot config file
    Bug Description:
    The value(URL) stored in the parameter fails to load after DUT is rebooted. It takes up the default URL that is there is the bootconfig file.
    
    Fix includes:
    Code changes done to ensure that the URL set gets reflected when DUT is rebooted and connection to that ACS URL is also established.
    
    signed off by: Subhradeep Nath <subhradeep.nath@libertyglobal.com>
    
    fix comparisons between a pointer and the character value 0
    
    Fix compiler warnings.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-embedded/DslhManagementServer/ccsp_management_server_pa_api.c
+++ source-embedded/DslhManagementServer/ccsp_management_server_pa_api.c
@@ -469,7 +469,10 @@ void ReadTr69TlvData()
                          * that the Session was established due to a change to the ACS URL.
                          * Then the event code "0 BOOTSTRAP" will be contained in the first Inform.
                         */
-			objectInfo[ManagementServerID].parameters[ManagementServerURLID].value = AnscCloneString(object2->URL);
+			if(objectInfo[ManagementServerID].parameters[ManagementServerURLID].value == NULL)
+			{
+				objectInfo[ManagementServerID].parameters[ManagementServerURLID].value = AnscCloneString(object2->URL);
+			}
 			//on Fresh bootup / boot after factory reset, if the URL is empty, set default URL value
                         if (object2->URL[0] == '\0')
 			{
@@ -485,7 +488,10 @@ void ReadTr69TlvData()
 			}
 			else
 			{
-				objectInfo[ManagementServerID].parameters[ManagementServerURLID].value = AnscCloneString(object2->URL);
+				if(objectInfo[ManagementServerID].parameters[ManagementServerURLID].value == NULL)
+				{
+					objectInfo[ManagementServerID].parameters[ManagementServerURLID].value = AnscCloneString(object2->URL);
+				}
 			}
 			// Here, we need to check what is the value that we got through boot config file and update TR69 PA
 			if(object2->EnableCWMP == 1)

339ab33173f1f382345c780568a27a02c08e0b35
mkamat <mkamat@libertyglobal.com>
2021-08-25 10:53:40 -0700

    fix CWMP Password if not defined via bootfile TLV
    When CWMP Password parameter is not defined in the bootfile TLV, CPE
    must use the default value for the CWMP Password (i.e an empty
    string) to establish a connection with the ACS. [MVXREQ-213]

--- source-embedded/DslhManagementServer/ccsp_management_server_pa_api.c
+++ source-embedded/DslhManagementServer/ccsp_management_server_pa_api.c
@@ -166,6 +166,7 @@ CcspManagementServer_InitCustom
         CCSP_STRING             sdmXmlFilename
     );
 
+#if 0
 /* Customizable default password generation, platform specific
  */
 extern ANSC_STATUS
@@ -174,6 +175,7 @@ CcspManagementServer_GenerateDefaultPassword
         CCSP_STRING             pDftPassword,
         PULONG                  pulLength
     );
+#endif
 
 #if defined (INTEL_PUMA7)
 //Intel Proposed RDKB Generic Bug Fix from XB6 SDK
@@ -1254,6 +1256,7 @@ CcspManagementServer_GetPassword
     }
     else 
     {
+#if 0
         char          DftPassword[72] = {0};
         ULONG         ulLength        = sizeof(DftPassword) - 1;
         ANSC_STATUS   returnStatus    = CcspManagementServer_GenerateDefaultPassword(DftPassword, &ulLength);
@@ -1275,6 +1278,9 @@ CcspManagementServer_GetPassword
 
             return  AnscCloneString(DftPassword);
         }
+#else
+        return  AnscCloneString("");
+#endif
     }
 }
 

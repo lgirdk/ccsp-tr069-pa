1642462511583373642e6735556749696136636b
mkamat <mkamate@libertyglobal.com>
2022-02-22 13:54:42 -0800

    drop unnecessary memset() after AnscAllocateMemory()
    The memory returned by AnscAllocateMemory() is already initialised.

--- source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_mpaif.c
+++ source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_mpaif.c
@@ -3939,14 +3939,6 @@ CcspCwmppoMpaGetParameterAttributes
                 returnStatus = ANSC_STATUS_RESOURCES;
                 goto EXIT2;
             }
-            else
-            {
-                AnscZeroMemory
-                    (
-                        pParamAttrArray, 
-                        sizeof(CCSP_CWMP_PARAM_ATTRIB) * ulTotalParamCount
-                    );
-            }
 
             pSLinkEntry = AnscQueueGetFirstEntry(&pFcGpaResNsList->NsList);
             while ( pSLinkEntry )                                   

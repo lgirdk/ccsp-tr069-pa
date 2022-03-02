eadf2d7081f9bae7ac85cbfcb6389d61092ada8d
Andre McCurdy <armccurdy@gmail.com>
2022-03-02 15:30:21 -0800

    drop unnecessary Tr069 specific wrappers for Ansc memory APIs - part 2
    The CcspManagementServer_XXX() wrapper functions complicate the code
    without adding any value. Remove them.
    
      find . -name "*.[ch]" | xargs sed -i 's/CcspManagementServer_CloneString/AnscCloneString/g'
      find . -name "*.[ch]" | xargs sed -i 's/CcspManagementServer_Allocate/AnscAllocateMemory/g'
      find . -name "*.[ch]" | xargs sed -i 's/CcspManagementServer_Free/AnscFreeMemory/g'
      find . -name "*.[ch]" | xargs sed -i 's/AnscCloneStringInComponent/CcspManagementServer_CloneStringInComponent/g'
      find . -name "*.[ch]" | xargs sed -i 's/AnscFreeMemoryParameterSetting/CcspManagementServer_FreeParameterSetting/g'
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-embedded/DslhManagementServer/ccsp_management_server.h
+++ source-embedded/DslhManagementServer/ccsp_management_server.h
@@ -270,9 +270,6 @@ int SendValueChangeSignal(
     int parameterID,
     const char * oldValue);
 
-//#define CcspManagementServer_Allocate(size) AnscAllocateMemoryCountSize(CcspManagementServer_ComponentName, size)
-//#define CcspManagementServer_Free(p) AnscFreeMemoryCountSize(CcspManagementServer_ComponentName, p)
-
 void CcspManagementServer_GetSingleParameterValue(
         int objectID,
         int parameterID,
@@ -355,30 +352,6 @@ char * CcspManagementServer_RemoveWhiteString
     const char * src
 );
 
-void * CcspManagementServer_Allocate
-(
-    size_t size
-);
-
-void CcspManagementServer_Free
-(
-    void *p
-);
-
-/* Malloc memory that will be freed by other modules.
-  */
-char * CcspManagementServer_CloneString
-    (
-    const char * src
-    );
-
-/* Malloc memory that will be freed by itself.
- */
-char * CcspManagementServer_CloneStringInComponent
-    (
-    const char * src
-    );
-
 /* Malloc memory that will be freed by other modules.
   */
 char * CcspManagementServer_MergeString
@@ -387,14 +360,6 @@ char * CcspManagementServer_MergeString
     const char * src2
     );
 
-/* Malloc memory that will be freed by itself.
- */
-char * CcspManagementServer_MergeStringInComponent
-    (
-    const char * src1,
-    const char * src2
-    );
-    
 ANSC_STATUS
 CcspManagementServer_RestoreDefaultValues
     (

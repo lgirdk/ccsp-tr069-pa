235a5896dd10c040e3d567a4f15b475c2d940eae
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2022-08-10 16:56:25 +0200

    fix DUT does not send Inform message with "4 Value Change" event code when ConnectionRequestURL value is changed ( CH7465SI-369 F3896SI-1169 OFW-2886 )
    [BUG DESCRIPTION]
    Observed value change signal on ConnectRequestURL parameter got processed after the connection with ACS already established with new URL. the event code would be seen in the second Inform after the change in URL or not seen at all as sometimes undelivered VC parameter from PSM gets discarded soon after the previous ongoing inform completed.
    
    [FIX DESCRIPTION]
    Remove dependency to process value change signal for ConnectionRequestURL via d-bus instead force event code into soap message when change in request url.
    
    [TESTING PERFORMED]
     (1) Bring up CPE in dual stack mode.
     (2) Set management server configuration.
     (3) Start tcpdump and capture packet on interface CPE communicating with ACS.
     (4) Block ipv6 CPE communication with ACS so that fallback happens to ipv4
     (5) Allow ipv6 and block ipv4 connection.
     (6) Stop tcpdump and verify pcap.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source-embedded/DslhManagementServer/ccsp_management_server.c
+++ source-embedded/DslhManagementServer/ccsp_management_server.c
@@ -1302,6 +1302,8 @@ extern void CcspManagementServer_GenerateConnectionRequestURLCustom(
     char *ipAddr,
     msObjectInfo *objectInfo);
 
+BOOL bAcsRequestURLChanged = FALSE;
+
 ANSC_STATUS CcspManagementServer_GenerateConnectionRequestURL(
     BOOL fromValueChangeSignal,
     char *newValue)
@@ -1489,6 +1491,7 @@ ANSC_STATUS CcspManagementServer_GenerateConnectionRequestURL(
 
         SendValueChangeSignal(ManagementServerID, ManagementServerConnectionRequestURLID, oldValue);
         CcspManagementServer_ValueChangeCB(CcspManagementServer_cbContext, CcspManagementServer_GetPAObjectID(ManagementServerID));
+        bAcsRequestURLChanged = TRUE;
         if(oldValue) AnscFreeMemory(oldValue);
     }
     

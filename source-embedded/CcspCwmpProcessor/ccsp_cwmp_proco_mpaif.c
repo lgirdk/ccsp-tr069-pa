f29ffbc8719e4c54a1fe134e57aae5494a48725f
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2023-03-20 18:16:48 +0100

    fix memory leak in CcspCwmppoMpaGetParameterValues
    Memory leak observed when GPV on Device. returned 9002 internal error.
    
    ==17174== 169,399 bytes in 3,505 blocks are definitely lost in loss record 408 of 410
    ==17174==    at 0x48435C0: calloc (vg_replace_malloc.c:762)
    ==17174==    by 0x4B34F45: CcspBaseIf_getParameterValues_rbus (ccsp_base_api.c:661)
    ==17174==    by 0x4B3507B: CcspBaseIf_getParameterValues (ccsp_base_api.c:716)
    ==17174==    by 0x1ED37: CcspCwmppoMpaGetParameterValues (ccsp_cwmp_proco_mpaif.c:2329)
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

--- source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_mpaif.c
+++ source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_mpaif.c
@@ -2542,11 +2542,16 @@ CcspCwmppoMpaGetParameterValues
             }
             
             free_parameterValStruct_t(pCcspCwmpCpeController->hMsgBusHandle, nParamCount, pParamValues);
-
+            pParamValues = NULL;
             if ( returnStatus != ANSC_STATUS_SUCCESS )
             {
                 break;
             }
+        }
+
+        if ( pParamValues )
+        {
+             free_parameterValStruct_t(pCcspCwmpCpeController->hMsgBusHandle, nParamCount, pParamValues);
         }
 
         if ( returnStatus != ANSC_STATUS_SUCCESS )
@@ -3142,12 +3147,17 @@ CcspCwmppoMpaGetParameterNames
         }
 
         free_parameterInfoStruct_t(pCcspCwmpCpeController->hMsgBusHandle, ParamInfoArraySize, ParamInfoArray);
-
+        ParamInfoArray = NULL;
         if ( returnStatus != ANSC_STATUS_SUCCESS )
         {
             break;
         }
     }
+
+        if ( ParamInfoArray )
+        {
+            free_parameterInfoStruct_t(pCcspCwmpCpeController->hMsgBusHandle, ParamInfoArraySize, ParamInfoArray);
+        }
 
         if ( ppFcNameArray )
         {
@@ -4157,12 +4167,18 @@ CcspCwmppoMpaGetParameterAttributes
                 }
 
                 free_parameterAttributeStruct_t(pCcspCwmpCpeController->hMsgBusHandle, nCcspAttrArraySize, ppCcspAttrArray);
+                ppCcspAttrArray = NULL;
             }
 
             if ( returnStatus != ANSC_STATUS_SUCCESS )
             {
                 break;
             }
+        }
+
+        if ( ppCcspAttrArray )
+        {
+            free_parameterAttributeStruct_t(pCcspCwmpCpeController->hMsgBusHandle, nCcspAttrArraySize, ppCcspAttrArray);
         }
 
         if ( returnStatus != ANSC_STATUS_SUCCESS )

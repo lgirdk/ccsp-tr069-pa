dd121f418ab44ef7c05aac0a3e0745b7a3d1f93d
Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>
2023-08-16 14:50:05 +0530

    TransferComplete packets are not seen in the DownloadRPC-Retry scenario ( OFW-4497 F5685SI-1298 )
    [Issue]
    
    TransferComplete packets are not seen for download rpc retry scenario after 1hr of retry.
    
    [Description]
    
    Transfercomplete packets are not seen for retry scenario due to if conditional check failure. Retry is happening for every minute till reach out the timeout value of 60mins and after that the iRetrycount was resetted to "0" because of that if conditional check fails since it is checking for iRetrycount isequalto "60".
    
    [Solution]
    
    Done changes in the conditional check to trigger the transfer complete after 1hr of retry for download rpc retry scenario.
    
    [Testing]
    
    1. Ensure DUT is online in ACS
    2. Trigger Download RPC when image located server is down
    3. CPE should retry to download firmware download
    4. Wait for an hour
    4. Take the packet capture
    5. Check the TransferComplete message and got correct code:<cwmp:TransferComplete>
                    <CommandKey/>
                    <FaultStruct>
                        <FaultCode>
                            9010
                            </FaultCode>
                        <FaultString>
                            Download failure
                            </FaultString>
                        </FaultStruct>
    
    Upstream Candidate: Maybe
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>

--- source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_event.c
+++ source-embedded/CcspCwmpProcessor/ccsp_cwmp_proco_event.c
@@ -1619,7 +1619,6 @@ static void *FWDWLD_retry_thrd(void *data)
         if(60 == iRetrycount)
         {
             CcspTraceWarning(("%s - Reached 1hr of retry\n",__FUNCTION__));
-            iRetrycount = 0;
             break;
         }
         sleep(50);
@@ -1917,7 +1916,7 @@ CcspCwmppoProcessPvcSignal
                     );
 #endif
 
-                if (!strcmp(val->newValue, "Retry"))
+                if ((!strcmp(val->newValue, "Retry")) && iRetrycount != 60)
                 {
                     if(iRetrycount == 0 )
                     {
@@ -1926,8 +1925,7 @@ CcspCwmppoProcessPvcSignal
                         pthread_detach(TCEvtHandle_tid);
                     }
                 }
-
-                if(!((!strcmp("Not Started",val->newValue)) || (!strcmp("In Progress",val->newValue)) || (!strcmp("Completed",val->newValue)) || !(iRetrycount == 60)))
+                else if(!((!strcmp("Not Started",val->newValue)) || (!strcmp("In Progress",val->newValue)) || (!strcmp("Completed",val->newValue))))
                 {			
                     pCommandKey = pCcspCwmpCpeController->LoadCfgFromPsm((ANSC_HANDLE)pCcspCwmpCpeController, "CommandKey");             
 

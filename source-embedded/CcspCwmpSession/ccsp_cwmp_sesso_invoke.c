9d74ee2f67d290ed97bf536a84d38b3e855913eb
SreejaSKumar <srekumar.contractor@libertyglobal.com>
2023-10-03 17:44:24 +0530

    Revert "fix "4 VALUE CHANGE" is always sent after each Reboot/Factory Reset (F3896SI-1442 CH7465SI-500 F5685SI-688 OFW-3450)"
    This reverts commit 51c390f1b5b91da973210baf656f7f66c80b17ac.
    
    DUT is not sending undelivered '4 VALUE CHANGE' passive notification across reboots along with event '1 BOOT'[OFW-4912,F3896SI-2068]
    
    BUG DESCRIPTION:
    DUT is not sending undelivered '4 VALUE CHANGE' passive notification across reboots along with event '1 BOOT'
    
    ROOT CAUSE:
    OFW doesnt have the implememtation to send undelivered 4 value change event along with BOOT .
    
    FIX DESCRIPTION:
    Reverted this change, as its no longer required.
    Get the undleivered 4 value change event from PSM and send along with the inform message of BOOT
    
    TESTING PERFORMED:
    1. Load the DUT with test build and ensure CM is operational
    2. Make sure passive notification is enabled on private 2.4G SSID
    3. Start packet capture between ACS and erouter interface
    4. Login to GUI and Navigate to Advanced Settings> Wireless> Security> 2.4 GHz Wireless Security Settings> WiFi Network Name (SSID)
    5. Modify the Private 2.4GHz SSID name and Apply changes
    6. Reboot the DUT
    7. Verify from capture that DUT sends INFORM message with event '4 VALUE CHANGE' for the data model parameter Device.WiFi.SSID.10001.SSID along with event '1 BOOT'
    
    Upstreamable : Yes
    
    Signed-off-by: Sreeja S Kumar (srekumar.contractor@libertyglobal.com)

--- source-embedded/CcspCwmpSession/ccsp_cwmp_sesso_invoke.c
+++ source-embedded/CcspCwmpSession/ccsp_cwmp_sesso_invoke.c
@@ -368,7 +368,6 @@ CcspCwmpsoInform
 
     BOOL                            bValChange           = FALSE;
     BOOL                            bBootStrap           = FALSE;
-    BOOL                            bBoot                = FALSE;
     BOOL                            bHasBoot             = FALSE;
     int                             fd;
     static int bFirstInform = 1;
@@ -379,8 +378,8 @@ CcspCwmpsoInform
     static char SerialNumber[100];
     static char HardwareVersion[100];
     static char SoftwareVersion[256];
-    errno_t     rc =  -1, rc1 = -1;
-    int         ind = -1, ind1 = -1;
+    errno_t     rc =  -1;
+    int         ind = -1;
     char*                           pIntAlias            = NULL;
     char*                           pExtAlias            = NULL;
 
@@ -645,7 +644,7 @@ else
 
     ulParamIndex = ulPresetParamCount;
 
-    // for "O BOOTSTRAP" and "1 BOOT" events all "4 VALUE CHANGE" should be suppressed. incase of "1 BOOT", undelivered event should be attempted for redelivery in next inform.
+    // for "O BOOTSTRAP", all parameters are treated as in their original values, thus all "4 VALUE CHANGE" should be suppressed.
     for( i = 0; i < pMyObject->EventCount; i ++)
     { 
         rc = strcmp_s(CCSP_CWMP_INFORM_EVENT_NAME_Bootstrap,strlen(CCSP_CWMP_INFORM_EVENT_NAME_Bootstrap),((PCCSP_CWMP_EVENT)pMyObject->EventArray[i])->EventCode,&ind);
@@ -655,17 +654,9 @@ else
             bBootStrap = TRUE;
             break;
         }
-
-        rc1 = strcmp_s(CCSP_CWMP_INFORM_EVENT_NAME_Boot,strlen(CCSP_CWMP_INFORM_EVENT_NAME_Boot),((PCCSP_CWMP_EVENT)pMyObject->EventArray[i])->EventCode,&ind1);
-        ERR_CHK(rc1);
-        if((!ind1) && (rc1 == EOK))
-        {
-            bBoot = TRUE;
-            break;
-        }
     }
 
-    if ( !bBootStrap || !bBoot )
+    if ( !bBootStrap )
     {
         /*
          * When the Inform call results from a change to one or more parameter values (due to cause
@@ -777,9 +768,7 @@ else
         }
     }
 
-    /* Suppress "4 VALUE CHANGE" cwmp event during "0 BOOTSTRAP", "1 BOOT" or when NO passive/active VALUE CHANGE to be reported.
-     * incase of "1 BOOT", undelivered events should be attempted for redelivery in the next inform */
-    if ( bBootStrap || bBoot || !bValChange )
+    if ( bBootStrap || !bValChange ) // "0 BOOTSTRAP" or NO passive/active VALUE CHANGE to be reported
     {
         pMyObject->DiscardCwmpEvent((ANSC_HANDLE)pMyObject, CCSP_CWMPSO_EVENTCODE_ValueChange);
 
@@ -1107,7 +1096,7 @@ bFirstInform = 0;
 		 }
 
 		
-		if (bAcsRequestURLChanged && !bBootStrap && !bBoot)
+		if (bAcsRequestURLChanged && !bBootStrap)
 		{
 			int m;
 			BOOL bEventPresent = FALSE;
@@ -1116,7 +1105,7 @@ bFirstInform = 0;
 
 			for (m = 0; m < pMyObject->EventCount; m++)
 			{
-				if (strcmp(((PCCSP_CWMP_EVENT)pMyObject->EventArray[m])->EventCode, CCSP_CWMP_INFORM_EVENT_NAME_ValueChange) == 0)
+				if (strcmp(((PCCSP_CWMP_EVENT)pMyObject->EventArray[m])->EventCode, CCSP_CWMP_INFORM_EVENT_NAME_ValueChange) == 0 || strcmp(((PCCSP_CWMP_EVENT)pMyObject->EventArray[m])->EventCode, CCSP_CWMP_INFORM_EVENT_NAME_Boot) == 0)
 				{
 					CcspTr069PaTraceDebug(("Skip. Not adding " CCSP_CWMP_INFORM_EVENT_NAME_ValueChange "\n"));
 					bEventPresent = TRUE;
